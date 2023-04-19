9d88999c34830ccd48e58b67cc1a5c651fd2dab2
mmmmr <ob12564@163.com>
2023-04-19 22:43:19 +0800

    feat(leetcode): 题目补充

--- leetcode-hot-100/15.三数之和.js
+++ leetcode-hot-100/15.三数之和.js
@@ -1,3 +1,14 @@
+// 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+
+// 你返回所有和为 0 且不重复的三元组。
+
+// 注意：答案中不可以包含重复的三元组。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode.cn/problems/3sum
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
 /**
  * @param {number[]} nums
  * @return {number[][]}
--- leetcode-hot-100/3.无重复字符的最长子串.js
+++ leetcode-hot-100/3.无重复字符的最长子串.js
@@ -1,3 +1,5 @@
+// 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+
 /**
  * @param {string} s
  * @return {number}
