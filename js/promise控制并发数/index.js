12bd8f3a6f605bc448640440d06ea5eb2d3a45d9
xuheng <ob12564@163.com>
2023-06-07 18:57:11 +0800

    nothing

---
+++ js/promise控制并发数/index.js
@@ -0,0 +1,50 @@
+// 并发：并发是多个任务同时交替的执行（因为cpu执行指令的速度非常之快，它可以不必按顺序一段代码一段代码的执行，这样效率反而更加低下），这样看起来就是一起执行的，所以叫并发。
+// 并行：可以理解为多个物理cpu或者有分布式系统，是真正的'同时'执行
+// 并发控制：意思是多个并发的任务，一旦有任务完成，就立刻开启下一个任务
+// 切片控制：将并发任务切片的分配出来，比如10个任务，切成2个片，每片有5个任务，当前一片的任务执行完毕，再开始下一个片的任务，这样明显效率没并发控制那么高了
+// 转自：JetTsang
+
+const tasks = new Array(10).fill(0).map((v, i) => {
+  return function task() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log(i + 1);
+        resolve(i + 1)
+      }, 3000);
+    })
+  }
+})
+
+// tasks.forEach(cb => {
+//   cb()
+// })
+
+// 并发控制函数 https://juejin.cn/post/7235574844435431461
+function concurrencyControl(tasks, limit, cb) {
+  const queue = tasks.slice();
+  let count = 0
+
+  const runTask = async () => {
+    while (limit) {
+      limit--
+      if (!queue.length) {
+        break
+      }
+      const task = queue.shift()
+      const result = await task()
+      count++
+      limit++
+      if (count === tasks.length) {
+        cb(result)
+        break
+      }
+      runTask()
+    }
+  }
+  return runTask
+}
+concurrencyControl(tasks, 10, (taskId) => {
+  console.log(`task ${taskId} finish！`)
+})()
+
+// todo 使用 primose.race
---
+++ 面试/vue.md
@@ -0,0 +1,20 @@
+#### Vue3和Vue2的响应式原理
+
+
+
+####  为什么要用Proxy，区别在哪里
+
+
+
+#### Vue3为什么要用Reflect
+
+
+
+#### `tree-shaking`、虚拟`DOM`优化、`Diff`算法优化
+
+
+
+#### Computed 原理
+
+
+
